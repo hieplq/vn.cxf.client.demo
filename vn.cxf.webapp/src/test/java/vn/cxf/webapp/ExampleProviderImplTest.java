34e046556e5a65545d3b3f7a3733b52fb4cd2792
hieplq <hieplq@hasuvimex.vn>
2020-07-02 00:43:42 +0700

    refine repository by don't use transitive

--- vn.cxf.webapp/src/test/java/vn/cxf/webapp/ExampleProviderImplTest.java
+++ vn.cxf.webapp/src/test/java/vn/cxf/webapp/ExampleProviderImplTest.java
@@ -4,8 +4,6 @@ import static org.junit.Assert.assertNotNull;
 
 import org.junit.Test;
 
-import vn.cxf.webapp.ExampleProviderImpl;
-
 public class ExampleProviderImplTest {
 	@Test
 	public void simple() {
